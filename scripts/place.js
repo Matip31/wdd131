aa10768b5ed443663fd9e9edae7cca5f7a674405
matip31 <matiasmiguel31@gmail.com>
2026-05-22 23:53:45 -0300

    maceio

---
+++ scripts/place.js
@@ -0,0 +1,15 @@
+const temperature=32;
+const windSpeed=10;
+function calculateWindChill(temp,speed){
+    return (
+        13.12 +
+        0.6215 * temp -
+        11.37 * Math.pow(speed, 0.16) +
+        0.3965 * temp * Math.pow(speed, 0.16)
+    ).toFixed(1);
+}
+let windChill="N/A"
+if(temperature<=10 && windSpeed > 4.8){
+    windChill = calculateWindChill(temperature,windSpeed) + " °C"
+}
+document.querySelector("#wind-chill").textContent=windChill;
